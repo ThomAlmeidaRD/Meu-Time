b01c4192fa26b2740e93c83e3e5820f7dd4367c4
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-18 19:05:39 -0300

    Autenticação com chave String

--- meu-time/src/App.jsx
+++ meu-time/src/App.jsx
@@ -1,5 +1,26 @@
 import './assets/css/main.css'
 
+var API_KEY = 'c0c99d1d10c719c858bcdcdf31134c83'
+
+function auth() {
+
+  var txtAPI = document.querySelector('input')
+  var displayError = document.querySelector('h3')
+
+  if (txtAPI.value == '') {
+    displayError.style.display = 'block'
+    displayError.innerText = 'Vazio!, é necessário uma Chave de API.'
+  } else if (txtAPI.value != API_KEY) {
+    displayError.style.display = 'block'
+    displayError.innerText = 'Insira uma chave de API válida.'
+  }else if(txtAPI.value == API_KEY){
+    displayError.style.display = 'block'
+    displayError.style.background = 'green'
+    displayError.style.color = 'springgreen'
+    displayError.innerText = 'Chave inserida com sucesso'
+  }
+}
+
 function App() {
 
   var print = document.querySelector('p')
@@ -13,7 +34,8 @@ function App() {
           <h1>Todos os Dados do seu Time Favorito com uma Interface Incrível</h1>
           <p>Veja estatísticas, classficação das ligas e muito mais do seu time preferido á pouquissimos cliques e não perca mais nada sobre o seu time.</p>
           <input type="text" placeholder='API KEY aqui' />
-          <button>Entrar</button>
+          <button onClick={auth}>Entrar</button>
+          <h3 id='display-error'>display error</h3>
         </div>
       </main>
     </>
