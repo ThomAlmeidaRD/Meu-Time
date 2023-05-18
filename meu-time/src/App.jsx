d77044b12b6bb0b9dd07fd46089426d131972747
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-18 18:11:41 -0300

    Fazendo as primeiras requisições

--- meu-time/src/App.jsx
+++ meu-time/src/App.jsx
@@ -2,10 +2,30 @@ import './assets/css/main.css'
 
 function App() {
 
+  var print = document.querySelector('p')
+
+  fetch("https://v3.football.api-sports.io/status", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "v3.football.api-sports.io",
+      "x-rapidapi-key": "c0c99d1d10c719c858bcdcdf31134c83"
+    }
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log(data)
+      print.innerHTML = data
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
   return (
     <>
       <h1>hello</h1>
-      <p>usando rotas para esta pagina</p>
+      <p>
+
+      </p>
     </>
   )
 }
