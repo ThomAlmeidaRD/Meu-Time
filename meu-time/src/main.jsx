0ecd874ad0dc7f15e35c3511c92ea857236ed4c5
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-31 12:19:06 -0300

    Coletando Trafego

--- meu-time/src/main.jsx
+++ meu-time/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const pages = createBrowserRouter([
   {
